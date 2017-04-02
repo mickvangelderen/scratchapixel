b9305af32b44315466b3dca5228ee494157b1720
Mick van Gelderen <mickvangelderen@gmail.com>
2017-04-02 15:11:50 +0200

    :sparkles: Compile shaders

--- rust_experiment/src/main.rs
+++ rust_experiment/src/main.rs
@@ -16,6 +16,35 @@ fn main() {
         gl::load_with(|symbol| window.get_proc_address(symbol) as *const _);
     }
 
+    // Create shaders.
+    let vertex_shader_source = file_to_string("assets/vertex_shader.glsl").unwrap();
+    let fragment_shader_source = file_to_string("assets/fragment_shader.glsl").unwrap();
+    unsafe {
+        let vertex_shader_id: u32 = gl::CreateShader(gl::VERTEX_SHADER);
+        gl::ShaderSource(
+            vertex_shader_id,
+            1,
+            [vertex_shader_source.as_ptr() as *const i8].as_ptr(),
+            [vertex_shader_source.len() as i32].as_ptr()
+        );
+        gl::CompileShader(vertex_shader_id);
+
+        let fragment_shader_id: u32 = gl::CreateShader(gl::FRAGMENT_SHADER);
+        gl::ShaderSource(
+            fragment_shader_id,
+            1,
+            [fragment_shader_source.as_ptr() as *const i8].as_ptr(),
+            [fragment_shader_source.len() as i32].as_ptr()
+        );
+        gl::CompileShader(fragment_shader_id);
+
+        let program_id: u32 = gl::CreateProgram();
+        gl::AttachShader(program_id, vertex_shader_id);
+        gl::AttachShader(program_id, fragment_shader_id);
+        gl::LinkProgram(program_id);
+        gl::UseProgram(program_id);
+    }
+
     let mut vertex_array_id: u32 = 0;
 
     unsafe { gl::GenVertexArrays(1, &mut vertex_array_id); }
@@ -97,4 +126,18 @@ fn main() {
         window.swap_buffers().unwrap();
         std::thread::sleep(std::time::Duration::from_millis(16));
     }
+}
+
+fn file_to_string<P: AsRef<std::path::Path>>(path: P) -> Result<String, std::io::Error> {
+    use std::fs::File;
+    use std::io::BufReader;
+    use std::io::Read;
+
+    File::open(path)
+        .and_then(|file| {
+            let mut contents = String::new();
+            BufReader::new(file)
+                .read_to_string(&mut contents)
+                .map(|_| contents)
+        })
 }
